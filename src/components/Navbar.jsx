73fc3f127af64e01151e1c71291c89382bc4e1f3
Muzhda Wafa <162709316+muzhiwa@users.noreply.github.com>
2025-09-05 07:26:16 +0000

    added responsive nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMoon,
+  faSun,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import "../styles/Navbar.css";
 
 const Navbar = ({ darkMode, toggleDarkMode }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const navLinks = [
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
@@ -12,16 +19,20 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
     { name: "Contact", path: "/contact" },
   ];
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="logo">Muzhda</div>
 
-        <div className="nav-links">
+        <div className="nav-links-desktop">
           {navLinks.map((link) => (
             <NavLink
-              key={link.name}
               to={link.path}
+              key={link.name}
               className={({ isActive }) =>
                 isActive ? "nav-link active" : "nav-link"
               }
@@ -31,16 +42,41 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
           ))}
         </div>
 
+        <div className="navbar-right">
+          <button
+            onClick={toggleDarkMode}
+            className="theme-toggle"
+            aria-label="Toggle dark mode"
+          >
+            <FontAwesomeIcon
+              icon={darkMode ? faSun : faMoon}
+              className="theme-color"
+            />
+          </button>
+        </div>
+
         <button
-          onClick={toggleDarkMode}
-          className="theme-toggle"
-          aria-label="Toggle dark mode"
+          className="navbar-toggler"
+          onClick={toggleMenu}
+          aria-label="Toggle navigation"
         >
-          <FontAwesomeIcon
-            icon={darkMode ? faSun : faMoon}
-            className="theme-color"
-          />
+          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
         </button>
+      </div>
+
+      <div className={`nav-links-mobile ${isMenuOpen ? "open" : ""}`}>
+        {navLinks.map((link) => (
+          <NavLink
+            to={link.path}
+            key={link.name}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+            onClick={() => setIsMenuOpen(false)}
+          >
+            {link.name}
+          </NavLink>
+        ))}
       </div>
     </nav>
   );
